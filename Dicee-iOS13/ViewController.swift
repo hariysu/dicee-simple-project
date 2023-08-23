412548f63e4ca5ffac5f37e5e2c604da5ccede6b
Abdurahman AYDIN <hariysu@gmail.com>
2023-08-23 11:27:36 +0300

    Increases/decreases dices with roll button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,19 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageView1.image = UIImage(named: "DiceSix")
-        //diceImageView1.alpha = 0.5
-        
-        diceImageView2.image = UIImage(named: "DiceTwo")
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //print("Button got tapped")
-        diceImageView1.image = UIImage(named: "DiceFour")
-        diceImageView2.image = UIImage(named: "DiceFour")
+        
+        diceImageView1.image = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][leftDiceNumber]
+        leftDiceNumber+=1
+        
+        diceImageView2.image = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][rightDiceNumber]
+        rightDiceNumber-=1
     }
     
 
